f5c496b683bb4ed2ed468e17158c298a71880425
viyyan <yuliantoalfian@rocketmail.com>
2021-09-07 16:32:28 +0700

    update port

--- src/rembg/cmd/server_v3.py
+++ src/rembg/cmd/server_v3.py
@@ -7,8 +7,8 @@ import argparse
 
 def main():
     ap = argparse.ArgumentParser()
-    log = "/usr/develop/rembg/log/rembg-server.log"
-    # log = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../log/rembg-server.log")
+    # log = "/usr/develop/rembg/log/rembg-server.log"
+    log = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../log/rembg-server.log")
     logging.basicConfig(filename=log,level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
     ap.add_argument(
@@ -27,9 +27,9 @@ def main():
         help="The port to bind to.",
     )
     args = ap.parse_args()
-    app.host = args.addr
-    app.port = args.port
     appl = factory.create_app(celery=app.celery)
+    appl.host = args.addr
+    appl.port = args.port
     appl.run()
     # serve(app, host=args.addr, port=args.port, threads=4)
 
